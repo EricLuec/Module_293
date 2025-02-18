0273f4b35b865b75142e91926242bceb1f1b4122
ericluec <eric.luecking@gmail.com>
2025-02-18 15:10:32 +0100

    js added

--- auftrag1/script.js
+++ auftrag1/script.js
@@ -1,6 +1,15 @@
-let startAnzahl = 100;
-let multiplicatorValue =  document.getElementById('fname').value;
+let input = document.querySelector("body");
+input.addEventListener("change", calc);
 
-document.getElementById("mehl").innerHTML = `${startAnzahl * multiplicatorValue}g Mehl`;
-document.getElementById("mehl").innerHTML = `${startAnzahl * multiplicatorValue}g Mehl`;
-document.getElementById("kaese").innerHTML = `${startAnzahl * multiplicatorValue}g Mehl`;
+function calc(e) {
+    let startAnzahl = 100;
+    let multiplicatorValue = document.getElementById("fname").value;
+
+    document.getElementById("mehl").innerHTML = `${startAnzahl * multiplicatorValue}g Mehl`;
+    document.getElementById("butter").innerHTML = `${startAnzahl * multiplicatorValue}g Butter`;
+    document.getElementById("kaese").innerHTML = `${startAnzahl * multiplicatorValue}g Käse`;
+    document.getElementById("personindicator").innerHTML = ` Dieses Rezept reicht für ${multiplicatorValue} Personen`;
+}
+    
+
+
