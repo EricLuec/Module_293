66c6dff74cf41ec68393a3b55e9c77ea070f6fe8
ericluec <eric.luecking@gmail.com>
2025-02-18 14:47:11 +0100

    js added

---
+++ auftrag1/script.js
@@ -0,0 +1,6 @@
+let startAnzahl = 100;
+let multiplicatorValue =  document.getElementById('fname').value;
+
+document.getElementById("mehl").innerHTML = `${startAnzahl * multiplicatorValue}g Mehl`;
+document.getElementById("mehl").innerHTML = `${startAnzahl * multiplicatorValue}g Mehl`;
+document.getElementById("kaese").innerHTML = `${startAnzahl * multiplicatorValue}g Mehl`;
