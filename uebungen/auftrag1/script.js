c0a6c7dd3f51958cdbb136a5286d537cd00cbe2c
ericluec <eric.luecking@gmail.com>
2025-05-06 09:06:07 +0200

    pulled project over from pc and finished for turning in

---
+++ uebungen/auftrag1/script.js
@@ -0,0 +1,12 @@
+let input = document.getElementById("calcButton");
+input.addEventListener("click", calc);
+
+function calc(e) {
+    let startAnzahl = 100;
+    let multiplicatorValue = document.getElementById("fname").value;
+
+    document.getElementById("mehl").innerHTML = `${startAnzahl * multiplicatorValue}g Mehl`;
+    document.getElementById("butter").innerHTML = `${startAnzahl * multiplicatorValue}g Butter`;
+    document.getElementById("kaese").innerHTML = `${startAnzahl * multiplicatorValue}g Käse`;
+    document.getElementById("personindicator").innerHTML = ` Dieses Rezept reicht für ${multiplicatorValue} Personen`;
+}
